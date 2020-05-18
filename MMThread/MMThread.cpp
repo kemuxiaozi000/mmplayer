988a10a19625f1139157db615a2a121d90c28e4f
kemuxiaozi000 <skcherryblossom@163.com>
2020-05-19 00:36:44 +0800

    cmakelist revise

--- MMThread/MMThread.cpp
+++ MMThread/MMThread.cpp
@@ -1,8 +1,8 @@
 ﻿# include "MMThread.h"
 # include <thread>
 
-int MMTHread::Start() {
-	std::thread t(run, this); // 加this表明是当前类对象的run方法
+int MMThread::Start() {
+	std::thread t(&MMThread::run, this); // run 是成员函数，要加this。加this表明是当前类对象的run方法
 	t.detach();
 	return 0;
 }
