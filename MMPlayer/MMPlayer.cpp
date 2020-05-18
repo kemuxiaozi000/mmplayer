2d4a1bf22ac1b6158c38e94b9cdba1ee4d69f90f
sunke <skcherryblossom@163.com>
2020-05-18 20:00:47 +0800

    2

--- MMPlayer/MMPlayer.cpp
+++ MMPlayer/MMPlayer.cpp
@@ -3,6 +3,8 @@
 
 #include "MMPlayer.h"
 #include <thread>
+#include <chrono>
+#include "MMThread/MMThread.h"
 using namespace std;
 
 void threadFunction(int threadIndex) {
@@ -11,15 +13,36 @@ void threadFunction(int threadIndex) {
 	}
 }
 
-int main()
-{	
+//int main()
+//{	
+//
+//	std::thread t1(threadFunction, 1);
+//	std::thread t2(threadFunction, 2);
+//	t1.join(); //阻塞 
+//	t2.join();
+//;	//t.detach(); //分离
+//	//t.joinable();
+//	
+//	return 0;
+//}
 
-	std::thread t1(threadFunction, 1);
-	std::thread t2(threadFunction, 2);
-	t1.join(); //阻塞 
-	t2.join();
-;	//t.detach(); //分离
-	//t.joinable();
+class MyMMThread : public MMThread
+{
+private:
+	int a = 0;
+public:
+	MyMMThread(int _a) {
+		a = _a;
+	}
+	virtual void run()
+	{
+		printf("MyMMThread %d\n", a);
+	}
+};
 
+int main() {
+	MyMMThread t(10);
+	t.Start();
+	std::this_thread::sleep_for(std::chrono::seconds(2));
 	return 0;
 }
---
+++ MMThread/MMThread.cpp
@@ -0,0 +1,8 @@
+﻿# include "MMThread.h"
+# include <thread>
+
+int MMTHread::Start() {
+	std::thread t(run, this); // 加this表明是当前类对象的run方法
+	t.detach();
+	return 0;
+}
